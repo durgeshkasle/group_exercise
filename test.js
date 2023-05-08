ac54dfa8a0c1e5c86cbc634c74b87f62b38cfac2
Munaf Hajir <munafhajirpro@gmail.com>
2023-05-08 11:37:46 +0530

    Added line

--- test.js
+++ test.js
@@ -3,8 +3,6 @@ console.log("Hello World Munaf");
 console.log("Hello World Rishikesh");
 console.log("Pawar");
 console.log("Kasle");
-<<<<<<< HEAD
 console.log("hello");
-=======
 console.log("Hi");
->>>>>>> 602e764a3f52a410daec7d5b2076e1a84f4ba32d
+console.log("Test");
