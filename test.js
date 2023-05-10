f8cda05c8a47b99924d5488ac815881ec4893e1a
Munaf Hajir <munafhajirpro@gmail.com>
2023-05-10 10:27:45 +0530

    Remove some lines of code

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
 console.log("Hello World Durgesh");
 console.log("Hello World Munaf");
 console.log("Hello World Rishikesh");
-console.log("Pawar");
-console.log("Kasle");
-console.log("hello");
-console.log("Hi");
-console.log("Test");
-console.log("Test 2");
